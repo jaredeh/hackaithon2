71c0b45dc9e0098fb3ce2d19a3b60ab2c8d1693d
Jared Hulbert <jaredeh@gmail.com>
2024-06-05 08:23:04 -0700

    updating venv with tinygrad and clean up

--- venv_create.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set up venv environment manually
-#  each shell needs to have `source venv/bin/activate` run at some time.
-
-python3 -m venv venv
-source venv/bin/activate
-python3 -m pip install -r requirements.txt
-
--- venv_setup.sh
+++ venv_setup.sh
@@ -7,3 +7,5 @@ python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+python3 -m pip install git+https://github.com/tinygrad/tinygrad.git
+echo "run \"source venv/bin/activate\" to activate venv"
